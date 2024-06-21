2bb64649d78ca3a427c0e40a7c6934e5940b061f
alok09 <aloke1456@gmail.com>
2024-06-21 13:38:24 +0530

    Calculating Wages for a Month

--- src/main/java/p1/Emp_wage.java
+++ src/main/java/p1/Emp_wage.java
@@ -6,29 +6,33 @@ public class Emp_wage
 {
     public static void main(String[] args)
     {
-        final int FULL_TIME=1;
-        final int PART_TIME=2;
+        final  int FULL_TIME=1;
+        final  int PART_TIME=2;
         final int EMP_RATE_PER_HR=250;
+        final int TOTAL_NO_OF_WORKING_DAYS=20;
 
-        int empHrs=0;
-        int empWage=0;
-
-        Random r=new Random();
-
-        int check=r.nextInt(0,3);
-        switch (check)
+        int empHrs=0,empWage=0,totalEmpWage=0;
+        for(int day=0;day<TOTAL_NO_OF_WORKING_DAYS;day++)
         {
-            case FULL_TIME :
-                empHrs=8;
-                break;
-            case PART_TIME:
-                empHrs=4;
-                break;
-            default://0 for absent
-                empHrs=0;
-                break;
+            Random r=new Random();
+            int check=r.nextInt(0,3);
+            switch (check)
+            {
+                case FULL_TIME :
+                    empHrs=8;
+                    break;
+                case PART_TIME:
+                    empHrs=4;
+                    break;
+                default://0 for absent
+                    empHrs=0;
+                    break;
+            }
+            empWage=empHrs*EMP_RATE_PER_HR;
+            totalEmpWage+=empWage;
+            System.out.println("Employee wage for day "+(day+1)+" is "+empWage);
         }
-        empWage=empHrs*EMP_RATE_PER_HR;
-        System.out.println("Employee wage="+empWage);
+        System.out.println("Total Employee wage in a month="+totalEmpWage);
     }
 }
+
