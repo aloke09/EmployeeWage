1d3220df7b2ab4866e2d8023d200b0142e806e48
alok09 <aloke1456@gmail.com>
2024-06-21 13:48:37 +0530

    Refactor the Code to write a Class Method to Compute Employee Wage - Use Class Method and Class Variables

--- src/main/java/p1/Emp_wage.java
+++ src/main/java/p1/Emp_wage.java
@@ -5,19 +5,19 @@ import java.util.Random;
 public class Emp_wage
 {
 
-    public static void main(String[] args)
-    {
-        final  int FULL_TIME=1;
-        final  int PART_TIME=2;
-        final int EMP_RATE_PER_HR=250;
-        final int TOTAL_NO_OF_WORKING_DAYS=20;
-        final int MAX_HRS_IN_MONTH=100;
+    final  int FULL_TIME=1;
+    final  int PART_TIME=2;
+    final int EMP_RATE_PER_HR=250;
+    final int TOTAL_NO_OF_WORKING_DAYS=20;
+    final int MAX_HRS_IN_MONTH=100;
 
+    public int computeEmpWage()
+    {
         int empHrs=0,no_of_days_worked=0;
         int totalWorkingHR=0;
         Random r=new Random();
         int check=0;
-        while(totalWorkingHR<=MAX_HRS_IN_MONTH && no_of_days_worked<TOTAL_NO_OF_WORKING_DAYS)
+        while(totalWorkingHR<MAX_HRS_IN_MONTH && no_of_days_worked<TOTAL_NO_OF_WORKING_DAYS)
         {
             no_of_days_worked++;
             check=r.nextInt(0,3);
@@ -36,8 +36,9 @@ public class Emp_wage
             totalWorkingHR=totalWorkingHR+empHrs;
             System.out.println("Day "+no_of_days_worked+" hour "+empHrs);
         }
-        System.out.println(totalWorkingHR);
+        System.out.println("Total working hours ->"+totalWorkingHR);
         int totalEmpWage=totalWorkingHR*EMP_RATE_PER_HR;
-        System.out.println("Total employee wage "+totalEmpWage);
+//        System.out.println("Total employee wage "+totalEmpWage);
+        return totalEmpWage;
     }
 }
