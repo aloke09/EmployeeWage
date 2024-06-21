877fc4377b21a44bbab546744b1a5749ee6afcea
alok09 <aloke1456@gmail.com>
2024-06-21 11:45:37 +0530

    uc3

--- src/main/java/p1/Emp_wage.java
+++ src/main/java/p1/Emp_wage.java
@@ -7,6 +7,7 @@ public class Emp_wage
     public static void main(String[] args) {
 
         final int FULL_TIME=1;
+        final int PART_TIME=2;
         final int EMP_RATE_PER_HR=250;
 
         int empHrs=0;
@@ -14,12 +15,17 @@ public class Emp_wage
 
         Random r=new Random();
 
-        int Check=r.nextInt(0,2);
-        if(Check==FULL_TIME)
+        int check=r.nextInt(0,3);
+        if(check==FULL_TIME)
         {
             empHrs=8;
         }
-        else {
+        else if(check==PART_TIME)
+        {
+            empHrs=4;
+        }
+        else
+        {
             empHrs=0;
         }
         empWage=empHrs*EMP_RATE_PER_HR;
